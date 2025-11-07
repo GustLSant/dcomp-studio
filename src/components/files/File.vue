8f26ea664369ba3e4bfae3d695e25369a540d0e7
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2025-11-06 22:01:57 -0300

    feat: created FIle and Folder UI component

---
+++ src/components/files/File.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+    import type { FileType } from '../../types/entities';
+    import { Icon } from '@iconify/vue';
+    import './entity.css';
+
+    const props = defineProps<{ file: FileType }>()
+</script>
+
+
+<template>
+    <div class="entity flex items-center gap-2 px-2 py-1 rounded-sm bg-gray-700 border border-white">
+        <Icon icon="mdi:file-outline" />
+        <p>{{ props.file.name }}</p>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/files/Folder.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+    import type { FolderType } from '../../types/entities';
+    import { Icon } from '@iconify/vue';
+    import './entity.css';
+
+    const props = defineProps<{ folder: FolderType }>()
+</script>
+
+
+<template>
+    <div class="entity flex items-center gap-2 px-2 py-1 rounded-sm bg-gray-700 border border-white">
+        <Icon icon="mdi:folder" />
+        <p>{{ props.folder.name }}</p>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
