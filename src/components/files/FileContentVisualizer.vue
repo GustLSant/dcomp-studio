5787891c598343ccca70fddd3fed2144b3069924
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2025-11-06 22:02:12 -0300

    feat: created FileCOntenetVisualizer

---
+++ src/components/files/FileContentVisualizer.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    import type { FileType, FolderType } from '../../types/entities';
+    import File from './File.vue';
+    import Folder from './Folder.vue';
+
+    const props = defineProps<{ folder: FolderType }>();
+</script>
+
+
+<template>
+    <div class="flex items-center gap-2 flex-wrap">
+        <div v-for="file in props.folder.content">
+            <Folder v-if="(Array.isArray(file.content))" :folder="(file as FolderType)" />
+            <File v-else :file="(file as FileType)" />
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
