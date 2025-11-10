fa38bbed7adc49e330ac740a3d47770b7b7d0202
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2025-11-09 22:31:33 -0300

    created navbar

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+  import Navbar from './components/layout/Navbar.vue';
+
   // npm install codemirror vue-codemirror --save
   // npm install @codemirror/lang-python @codemirror/lang-html @codemirror/lang-markdown
   // npm install @codemirror/theme-one-dark
@@ -7,8 +9,8 @@
 
 
 <template>
-  <div>
-    <p class="text-black">App renderizado!</p>
+  <div class="p-4 pt-[calc(52px+16px)] bg-(--background) h-screen dark">
+    <Navbar />
     <router-view />
   </div>
 </template>
---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    import HoverableIcon from '../common/HoverableIcon.vue';
+
+
+</script>
+
+
+<template>
+    <div class="fixed top-0 left-0 right-0 flex items-center justify-between p-2 px-4 bg-(--foreground) shadow-md">
+        <div class="flex flex-col leading-tight">
+            <p>Dcomp</p>
+            <p class="text-xs">Studio</p>
+        </div>
+
+        <HoverableIcon icon="mdi:menu" :size="24" />
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
