d5ddabbb9f611b5931591622cdf3eb17a13f5713
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2025-11-12 19:51:25 -0300

    import new logo

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
     import HoverableIcon from '../common/HoverableIcon.vue';
-
+    import Logo from '../../assets/logo/new_white.png'
 
 </script>
 
 
 <template>
-    <div class="fixed top-0 left-0 right-0 flex items-center justify-between p-2 px-4 bg-(--foreground) shadow-md">
-        <div class="flex flex-col leading-tight">
-            <p>Dcomp</p>
-            <p class="text-xs">Studio</p>
+    <div class="fixed top-0 left-0 right-0 z-50 flex items-center justify-between p-2 px-4 bg-(--foreground) shadow-md">
+        <div class="flex items-center gap-1.5">
+            <img :src="Logo" class="w-8" />
+            <div class="flex flex-col leading-tight">
+                <p>Dcomp</p>
+                <p class="text-xs -mt-1">Studio</p>
+            </div>
         </div>
 
         <HoverableIcon icon="mdi:menu" :size="24" />
