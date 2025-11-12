8bc4506706354723393d3acc370c1d3f5d6505d8
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2025-11-12 19:52:53 -0300

    added file and folder routes

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory, type RouteRecordRaw  } from "vue-router";
 import HomeView from "./views/HomeView.vue";
+import FolderView from "./views/FolderView.vue";
+import FileView from "./views/FileView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
     component: HomeView
+  },
+  {
+    path: "/folder/:id",
+    name: "Folder",
+    component: FolderView,
+  },
+  {
+    path: "/file/:id",
+    name: "File",
+    component: FileView,
   },
 ];
 
