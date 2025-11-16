9d86d6f328182f9229e54b8d36975c41fe0cc956
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2025-11-16 15:11:47 -0300

    created pulsing grid background

---
+++ src/components/pulsingGrid/GridElement.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+    const props = defineProps<{ maxOpacity: number }>();
+    const size: string = '64px';
+</script>
+
+
+<template>
+    <div :style="{width: size, height: size, opacity: `${props.maxOpacity}%`}" class="grid-element border-2 rounded-md">
+    </div>
+</template>
+
+
+<style scoped>
+    .grid-element {
+        animation-name: Pulse;
+        animation-duration: 2.0s;
+        animation-iteration-count: infinite;
+    }
+
+    @keyframes Pulse {
+        0%{
+            border-color: rgba(255, 255, 255, 0.0);
+        }
+        50%{
+            border-color: rgba(255, 255, 255, 1.0);
+        }
+        100%{
+            border-color: rgba(255, 255, 255, 0.0);
+        }
+    }
+</style>
---
+++ src/components/pulsingGrid/PulsingGridComponent.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+    import GridElement from './GridElement.vue';
+
+    const rowsCount: number = 24;
+    const columnsCount: number = 16;
+</script>
+
+
+<template>
+    <div class="pulsing-grid absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex flex-col gap-4 bg-black">
+        <div v-for="_count in rowsCount" class="flex gap-4">
+            <GridElement v-for="_count2 in columnsCount" :style="{ animationDelay: `${Math.random()*10.0}s` }" :max-opacity="Math.round(Math.random() * 100)" />
+        </div>
+    </div>
+
+    <div class="inner-shadow absolute top-0 bottom-0 left-0 right-0">
+    </div>
+</template>
+
+
+<style scoped>
+    .pulsing-grid {
+        transform-style: preserve-3d;
+        /* perspective: 1000px; */
+        transform: rotateX(-45deg) rotateY(15deg);
+    }
+
+    .inner-shadow{
+        box-shadow: 0px 0px 40px 40px rgba(0,0,0, 0.75) inset;
+    }
+</style>
