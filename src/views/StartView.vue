f3a3e690777a422687d44036674b9789ffad62f3
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2025-11-16 15:11:34 -0300

    fade-out effect

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,22 +1,24 @@
 <script setup lang="ts">
     import { useRouter } from 'vue-router';
     import Logo from '../assets/logo/new_white.png';
-import PulsingGridComponent from '../components/pulsingGrid/PulsingGridComponent.vue';
+    import PulsingGridComponent from '../components/pulsingGrid/PulsingGridComponent.vue';
+    import { ref } from 'vue';
 
+    const isExiting = ref<boolean>(false)
     const router = useRouter();
 
     function handleClickContinue() {
-        router.push('/home')
-        console.log()
+        isExiting.value = true;
+        setTimeout(() => { router.push('/home'); }, 600);
     }
 </script>
 
 
 <template>
-    <div class="relative flex flex-col min-h-screen p-4">
+    <div class="relative flex flex-col min-h-screen p-4" :class="(isExiting) ? 'fade-out-short' : ''" style="animation-delay: 0.3s;">
         <PulsingGridComponent />
 
-        <div class="flex items-center gap-2 fade-in-left-normal">
+        <div class="flex items-center gap-2" :class="(isExiting) ? 'fade-out-left-normal' : 'fade-in-left-normal'">
             <img :src="Logo" alt="Logo" class="w-16 h-16" />
             <div class="flex flex-col font-mono font-light">
                 <p class="text-4xl">Dcomp</p>
@@ -30,7 +32,7 @@ import PulsingGridComponent from '../components/pulsingGrid/PulsingGridComponent
             <p class="font-light opacity-30">Clique para continuar</p>
         </div>
 
-        <div class="max-w-[80%] self-end text-right flex flex-col items-end gap-2 fade-in-right-normal" style="animation-duration: 0.6s;">
+        <div class="max-w-[80%] self-end text-right flex flex-col items-end gap-2" :class="(isExiting) ? 'fade-out-right-normal' : 'fade-in-right-normal'" :style="{ animationDuration: (!isExiting) ? '0.6s' : '0.3s' }">
             <p class="max-w-60 text-3xl font-light">IDE MOBILE RÁPIDA E ACESSÍVEL PARA PROGRAMAÇÃO E ESTUDO USANDO PYTHON</p>
             <p class="text-xs font-light opacity-50">*Também disponível offline <a class="underline" href="https://www.google.com/search?q=como+instalar+pwa&ie=UTF-8" target="_blank">(instale a versão PWA)</a></p>
         </div>
