44c3a32991aade65ccdf475b6c44a510e7de63c1
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2025-12-19 00:27:34 -0300

    refactor history state

--- src/components/actionModals/ActionModalContainer.vue
+++ src/components/actionModals/ActionModalContainer.vue
@@ -1,54 +1,84 @@
 <script setup lang="ts">
     import { onMounted, onUnmounted, ref } from 'vue';
-    import LoadingOverlay from '../common/LoadingOverlay.vue';
-
+    import { useRoute } from 'vue-router';
+    import { watch } from 'vue';
+    
     const modalContentRef = ref<HTMLElement | null>(null);
+    const modalName = ref<string>('actionModal');
     const isOpen = ref<boolean>(false);
-    const isLoading = ref<boolean>(false);
+    const route = useRoute();
 
-    onMounted(() => { window.addEventListener('popstate', onPopState); });
-    onUnmounted(() => { window.removeEventListener('popstate', onPopState); });
+    onMounted(() => { window.addEventListener('popstate', syncStateAndVisibility); });
+    onUnmounted(() => { window.removeEventListener('popstate', syncStateAndVisibility); });
+
+
+    watch(() => route.fullPath, clearModalFromHistory);
+
+
+    function setModalName(_newName: string): void { modalName.value = _newName; }
+
+
+    function requestModalOpen() {
+        const isModalAlreadyOpen: boolean = history.state?.[modalName.value];
+        if (isModalAlreadyOpen) return;
+
+        history.pushState({ ...history.state, [modalName.value]: true }, '');
+        syncStateAndVisibility(); // pq o pushState nao emit o evento popState
+    }
+
+
+    function requestModalClose() {
+        if (history.state?.[modalName.value]) { history.back(); }
+    }
+
+
+    function syncStateAndVisibility() {
+        const shouldBeOpen = !!history.state?.[modalName.value];
+
+        if (shouldBeOpen) { openModal(); }
+        else { closeModal(); }
+    }
+
 
     function openModal() {
-        history.pushState({ modal: true }, ''); // empilha estado no histórico
-        document.body.style.overflow = "hidden";
         isOpen.value = true;
+        document.body.style.overflow = 'hidden';
     }
 
     function closeModal() {
-        if (history.state?.modal) { history.back(); } // Remove o estado do histórico sem voltar página
-        document.body.style.overflow = "";
-        isOpen.value = false;
-    }
-    
-    
-    function onPopState(event: PopStateEvent) {
-        if (isOpen.value) {
-            event.preventDefault(); // Intercepta o botão voltar
-            isOpen.value = false;
-        }
+        isOpen.value = false
+        document.body.style.overflow = ''
     }
 
 
-    const handleClickOutsideModal = (_event: MouseEvent) => {
+    function handleClickOutsideModal (_event: MouseEvent) {
         if(!modalContentRef.value) return;
-        if(!modalContentRef.value.contains(_event.target as Node)){ closeModal(); }
+        if(!modalContentRef.value.contains(_event.target as Node)){ requestModalClose(); }
+    }
+
+
+    function clearModalFromHistory() {
+        if (isOpen.value) {
+            const newState = { ...history.state };
+            delete newState[modalName.value];
+
+            history.replaceState(newState, '');
+            closeModal();
+        }
     }
 
 
     defineExpose({
         isOpen,
-        isLoading,
-        openModal,
-        closeModal,
+        setModalName,
+        requestModalOpen,
+        requestModalClose,
     })
 </script>
 
 
 <template>
     <div v-if="isOpen" @click="handleClickOutsideModal" class="z-50 fixed top-0 bottom-0 left-0 right-0 flex items-center justify-center p-2 bg-black/30 backdrop-blur-sm">
-        <LoadingOverlay v-if="isLoading" />
-
         <div ref="modalContentRef" class="grow max-w-(--max-modal-width)">
             <slot />
         </div>
