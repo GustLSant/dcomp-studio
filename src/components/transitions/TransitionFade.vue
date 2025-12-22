e2ec584b68b9b9aa43e9dd61f245424952635092
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2025-12-22 16:23:58 -0300

    created transition elements

---
+++ src/components/transitions/TransitionFade.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+    <Transition name="fade">
+        <slot />
+    </Transition>
+</template>
+
+
+<style scoped>
+    .fade-enter-to, .fade-leave-from {
+        opacity: 100%;
+    }
+
+    .fade-enter-active, .fade-leave-active {
+        transition: opacity var(--anim-duration-short);
+    }
+
+    .fade-enter-from {
+        opacity: 0%;
+    }
+
+    .fade-leave-to {
+        opacity: 0%;
+    }
+</style>
---
+++ src/components/transitions/TransitionFadeHorizontal.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+    <Transition name="fade-horizontal">
+        <slot />
+    </Transition>
+</template>
+
+
+<style scoped>
+    .fade-horizontal-enter-to, .fade-horizontal-leave-from {
+        opacity: 100%;
+        transform: translateX(0px);
+    }
+
+    .fade-horizontal-enter-active, .fade-horizontal-leave-active {
+        transition: opacity var(--anim-duration-short), transform var(--anim-duration-short);
+    }
+
+    .fade-horizontal-enter-from {
+        opacity: 0%;
+        transform: translateX(-30px);
+    }
+
+    .fade-horizontal-leave-to {
+        opacity: 0%;
+        transform: translateX(30px);
+    }
+</style>
