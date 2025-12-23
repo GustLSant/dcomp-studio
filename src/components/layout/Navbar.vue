4ca767edc2f1a6eedaa5821256d5fb3946d5b916
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2025-12-23 17:54:38 -0300

    added link

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -8,13 +8,13 @@
 <template>
     <ShinyContainer class="p-0! pb-px! fixed top-0 left-0 right-0 z-30 shadow-md backdrop-brightness-50 max-w-(--max-app-width) m-auto">
         <div class="flex items-center justify-between p-2 px-4 bg-(--foreground)">
-            <div class="flex items-center gap-1.5">
+            <RouterLink to="/home" class="flex items-center gap-1.5">
                 <img :src="Logo" class="w-8" />
                 <div class="flex flex-col leading-tight font-mono">
                     <p>Dcomp</p>
                     <p class="text-xs -mt-1">Studio</p>
                 </div>
-            </div>
+            </RouterLink>
     
             <HoverableIcon icon="mdi:menu" :size="24" />
         </div>
