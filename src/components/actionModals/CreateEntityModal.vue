ba1871614591e046ed116010585b6ca246c3a406
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2025-12-23 17:56:51 -0300

    update to use new ActionModal

--- src/components/actionModals/CreateEntityModal.vue
+++ src/components/actionModals/CreateEntityModal.vue
@@ -3,29 +3,27 @@
     import { Icon } from '@iconify/vue';
     import { addFile } from '../../services/files';
     import { addFolder } from '../../services/folders';
-    import type { EntityKind, FileType, FolderType } from '../../types/entities';
+    import type { Entity, EntityKind, FileType, FolderType } from '../../types/entities';
     import { getDefaultFile, getDefaultFolder } from '../../utils/entities';
     import { createPopup } from '../../utils/popup';
     import FolderPreview from '../folders/FolderPreview.vue';
     import Button from '../common/Button.vue';
-    import CloseButton from '../common/CloseButton.vue';
     import ShinyContainer from '../common/shinyContainer/ShinyContainer.vue';
     import TextInput from '../common/TextInput.vue';
     import eventBus from '../../eventBus';
     import { EVENT_CREATE_ENTITY } from '../../events/actionModal';
     import { EVENT_ENTITY_TREE_UPDATED } from '../../events/entitiesTree';
-    import { useRouter } from 'vue-router';
-    import LoadingOverlay from '../common/LoadingOverlay.vue';
+    import { useRoute, useRouter } from 'vue-router';
     import ActionModalContainer from './ActionModalContainer.vue';
 
     const modalRef = ref<InstanceType<typeof ActionModalContainer> | null>(null);
+    const router = useRouter();
+    const route = useRoute();
+
     const entityKind = ref<EntityKind>('file');
     const parentFolder = ref<FolderType | undefined>(undefined);
     const entityName = ref<string>('');
-    const loading = ref<boolean>(false);
-    const router = useRouter();
 
-    onMounted(() => { modalRef.value?.setModalName('createEntityModal'); });
 
     onMounted(() => { eventBus.addEventListener(EVENT_CREATE_ENTITY, handleOpenModal) });
     onUnmounted(() => { eventBus.removeEventListener(EVENT_CREATE_ENTITY, handleOpenModal) });
@@ -41,75 +39,62 @@
         entityName.value = (event.detail.entityKind === 'file') ? 'Novo Arquivo' : 'Nova Pasta';
 
         if (!parentFolder.value) {
-            createPopup('error', 'Erro ao criar entidade', 'Por favor, tente novamente');
+            createPopup('error', `Erro ao criar ${(entityKind.value === 'file') ? 'Arquivo' : 'Pasta'}`, 'Por favor, tente novamente');
             return ;
         }
 
-        modalRef.value!.requestModalOpen();
-    }
-
-
-    function handleClickAddButton() {
-        if (entityKind.value === 'file') handleAddFile();
-        else handleAddFolder();
-    }
-
-
-    async function handleAddFolder() {
-        const newFolder: FolderType = getDefaultFolder();
-        newFolder.parentFolderId = parentFolder.value!.id!;
-        newFolder.name = entityName.value;
-
-        loading.value = true;
-
-        addFolder(newFolder)
-        .then((_response: number) => {
-            createPopup('success', 'Sucesso', 'Sucesso ao criar a pasta');
-            eventBus.dispatchEvent(new Event(EVENT_ENTITY_TREE_UPDATED));
-
-            const id: number = _response;
-            router.push({ path: '/folder/' + id });
-        })
-        .catch((_error) => { createPopup('error', 'Erro ao criar nova pasta', 'Por favor, tente novamente'); })
-        .finally(() => { loading.value = false; })
-    }
-
-
-    async function handleAddFile() {
-        const newFile: FileType = getDefaultFile();
-        newFile.parentFolderId = parentFolder.value!.id!;
-        newFile.name = entityName.value;
-
-        loading.value = true;
-
-        addFile(newFile)
-        .then((_response) => {
-            createPopup('success', 'Sucesso', 'Sucesso ao criar o arquivo');
-            eventBus.dispatchEvent(new Event(EVENT_ENTITY_TREE_UPDATED));
-
-            const id: number = _response;
-            router.push({ path: '/file/' + id });
-        })
-        .catch((_error) => { createPopup('error', 'Erro ao criar arquivo', 'Por favor, tente novamente'); })
-        .finally(() => { loading.value = false; })
+        modalRef.value!.openModal();
     }
 
 
     function handleCloseModal() {
-        modalRef.value!.requestModalClose();
+        modalRef.value!.closeModal();
+    }
+
+
+    function handleAddEntity() {
+        let newEntity: Entity;
+
+        if (entityKind.value === 'file') newEntity = getDefaultFile();
+        else newEntity = getDefaultFolder();
+
+        newEntity.parentFolderId = parentFolder.value!.id!;
+        newEntity.name = entityName.value;
+
+        modalRef.value?.setLoading(true);
+
+        if (entityKind.value === 'file') {
+            addFile(newEntity as FileType)
+            .then((_response) => { performSuccessEffect(_response); })
+            .catch((_error)   => { createPopup('error', 'Erro ao criar arquivo', 'Por favor, tente novamente'); })
+            .finally(()       => { modalRef.value?.setLoading(false); })
+        }
+        else {
+            addFolder(newEntity as FolderType)
+            .then((_response: number) => { performSuccessEffect(_response); })
+            .catch((_error)           => { createPopup('error', 'Erro ao criar nova pasta', 'Por favor, tente novamente'); })
+            .finally(()               => { modalRef.value?.setLoading(false); })
+        }
+    }
+
+
+    function performSuccessEffect(_id: number) {
+        const popupSubtitle: string = (entityKind.value === 'file') ? 'Sucesso ao criar o arquivo' : 'Sucesso ao criar a pasta';
+
+        createPopup('success', 'Sucesso', popupSubtitle);
+        eventBus.dispatchEvent(new Event(EVENT_ENTITY_TREE_UPDATED));
+        
+        handleCloseModal();
+        if (route.name !== 'FileMenu') { router.push({ path: `/${entityKind.value}/${_id}`}); }
     }
 </script>
 
 
 <template>
-    <LoadingOverlay v-if="loading" />
-
     <ActionModalContainer ref="modalRef">
         <ShinyContainer class="rounded-md max-w-[600px] m-auto">
 
             <div class="relative flex flex-col gap-4 bg-(--foreground) p-2 rounded-md">
-                <CloseButton @click="handleCloseModal" />
-
                 <div class="flex items-center gap-1 text-xl">
                     <Icon icon="mdi:add" width="24" height="24" />
                     <p>{{ (entityKind === 'file') ? 'Criar novo arquivo' : 'Criar nova pasta' }}</p>
@@ -133,7 +118,7 @@
                         Cancelar
                     </Button>
     
-                    <Button @click="handleClickAddButton" variant="primary-filled" icon="mdi:add">
+                    <Button @click="handleAddEntity" variant="primary-filled" icon="mdi:add">
                         Criar
                         {{ (entityKind === 'file') ? 'Arquivo' : 'Pasta' }}
                     </Button>
