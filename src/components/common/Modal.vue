01278fa9c5602f663f47bca99265fe2cc05b5754
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2025-12-26 21:21:40 -0300

    se mudar rota, modal fecha

--- src/components/common/Modal.vue
+++ src/components/common/Modal.vue
@@ -2,6 +2,7 @@
     import { ref, watch } from 'vue';
     import CloseButton from './CloseButton.vue';
     import TransitionFadeHorizontal from '../transitions/TransitionFadeHorizontal.vue';
+    import { useRoute } from 'vue-router';
 
     const props = withDefaults(defineProps<{ open?: boolean, persistent?: boolean }>(), {
         open: true,
@@ -9,18 +10,24 @@
     });
 
     const modalContentRef = ref<HTMLElement | null>(null);
+    const route = useRoute();
     const emit = defineEmits(['close']);
 
     watch(() => props.open, handleOpenChange);
+    watch(() => route.fullPath, handleRouteChange);
 
     function handleOpenChange() {
         if (props.open) { document.body.style.overflow = "hidden"; }
         else { document.body.style.overflow = ""; }
     }
 
-    const handleClickOutsideModal = (_event: MouseEvent) => {
-        if(props.persistent || !modalContentRef.value) return;
-        if(!modalContentRef.value.contains(_event.target as Node)){ emit('close'); }
+    function handleClickOutsideModal(_event: MouseEvent) {
+        if (props.persistent || !modalContentRef.value) return;
+        if (!modalContentRef.value.contains(_event.target as Node)) { emit('close'); }
+    }
+
+    function handleRouteChange() {
+        if (props.open) {  emit('close'); }
     }
 </script>
 
