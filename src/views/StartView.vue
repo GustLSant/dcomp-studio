d1a9c209921f8d1e8aa49cb44492d7e54d716a51
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2025-12-27 23:40:58 -0300

    fix text bottom

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -25,14 +25,10 @@
                 <p class="text-2xl -mt-2">Studio</p>
             </div>
         </div>
+    </div>
 
-        <div class="grow"></div>
-
-        <div @click="handleClickContinue" class="absolute top-0 left-0 w-full h-full flex items-center justify-center">
-            <p class="font-light opacity-30">Clique para continuar</p>
-        </div>
-
-        <div class="max-w-[80%] absolute bottom-4 right-4 text-right flex flex-col items-end gap-2" :class="(isExiting) ? 'fade-out-right-normal' : 'fade-in-right-normal'" :style="{ animationDuration: (!isExiting) ? '0.6s' : '0.3s' }">
+    <div class="fixed bottom-0 left-0 right-0 p-2 max-w-(--max-app-width) m-auto flex justify-end">
+        <div class="max-w-[80%] text-right flex flex-col items-end gap-2" :class="(isExiting) ? 'fade-out-right-normal' : 'fade-in-right-normal'" :style="{ animationDuration: (!isExiting) ? '0.6s' : '0.3s' }">
             <p class="max-w-60 text-3xl font-light">IDE MOBILE RÁPIDA E ACESSÍVEL PARA PROGRAMAÇÃO E ESTUDO USANDO PYTHON</p>
             <p class="text-xs font-light opacity-50">*Também disponível offline <a class="underline" href="https://www.google.com/search?q=como+instalar+pwa&ie=UTF-8" target="_blank">(instale a versão PWA)</a></p>
         </div>
