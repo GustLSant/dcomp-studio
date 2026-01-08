0b43e5176ee4a8e001596a34cb642b6339084dc2
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2026-01-08 19:56:16 -0300

    removed old footer functions

--- src/components/codeEditor/CodeEditor.vue
+++ src/components/codeEditor/CodeEditor.vue
@@ -3,11 +3,8 @@
     import { Codemirror } from 'vue-codemirror';
     import { python } from "@codemirror/lang-python";
     import { codeThemesDict, getEditorTheme } from '../../utils/editorTheme';
-    import { onMounted, onUnmounted } from 'vue';
+    import { onMounted } from 'vue';
     import eventBus from '../../eventBus';
-    import { editorButtonsMappingToFunctions } from '../../utils/codeEditor';
-    import { codeEditorButtonItems, type CodeEditorButton } from '../../enums/codeEditorButtons';
-    import { FOOTER_BT_EVENT_SULFIX } from '../../utils/codeEditor';
     import { history } from "@codemirror/commands";
     import { EVENT_EDITOR_THEME_CHANGED } from '../../events/editor';
     import type { EditorTheme } from '../../enums/editorThemes';
@@ -27,37 +24,7 @@
 
     function onCodeMirrorReady(payload: { view: any }) { codeMirrorTextElement.value = payload.view; }
 
-
-    const footerButtonFunctions: (() => void)[] = [
-        ...codeEditorButtonItems.map((bt: CodeEditorButton) => {
-            return () => { editorButtonsMappingToFunctions[bt](codeMirrorTextElement.value) };
-        })
-    ]
-
-    onMounted(connectButtonEvents);
-    onUnmounted(disconnectButtonEvents);
-
     onMounted(() => { eventBus.addEventListener(EVENT_EDITOR_THEME_CHANGED, handleEditorThemeChanged); });
-
-
-    function connectButtonEvents() {
-        codeEditorButtonItems.forEach((bt: CodeEditorButton, idx: number) => {
-            eventBus.addEventListener(
-                bt + FOOTER_BT_EVENT_SULFIX,
-                footerButtonFunctions[idx]!
-            );
-        });
-    }
-
-    function disconnectButtonEvents() {
-        codeEditorButtonItems.forEach((bt: CodeEditorButton, idx: number) => {
-            eventBus.removeEventListener(
-                bt + FOOTER_BT_EVENT_SULFIX,
-                footerButtonFunctions[idx]!
-            );
-        });
-    }
-
 
     function handleEditorThemeChanged() {
         editorTheme.value = getEditorTheme();
