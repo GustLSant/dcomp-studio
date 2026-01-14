264f740798027220f4121ec2f519b931aa360a89
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2026-01-13 22:25:22 -0300

    fix: editor blur on footer click

--- src/components/file/EditorFooter.vue
+++ src/components/file/EditorFooter.vue
@@ -34,21 +34,23 @@
 
 
 <template>
-    <footer ref="footerRef" class="fixed bottom-0 left-0 right-0  flex items-stretch px-2  max-w-(--max-app-width) m-auto pointer-events-none">
+    <footer ref="footerRef" @pointerdown.prevent class="fixed bottom-0 left-0 right-0  flex items-stretch px-2  max-w-(--max-app-width) m-auto pointer-events-none">
         <div :class="(isOpen) ? 'ml-0' : 'ml-[calc(100%-30px)]'" class="w-full flex items-center gap-2 pb-1.5 pointer-events-auto">
-            <ShinyContainer class="rounded-full">
+            
+            <ShinyContainer class="rounded-full" @pointerdown.stop @click="toggleFooter">
                 <div class="rounded-full bg-(--foreground)">
-                    <HoverableIcon @click="toggleFooter" :class="(isOpen) ? '-scale-x-100' : 'scale-x-100'" icon="mdi:chevron-left" :size="20" />
+                    <HoverableIcon :class="(isOpen) ? '-scale-x-100' : 'scale-x-100'" icon="mdi:chevron-left" :size="20" />
                 </div>
             </ShinyContainer>
             
             <ShinyContainer class="grow overflow-x-hidden rounded-md">
                 <div class="overflow-x-auto flex items-center gap-2 py-1 px-2 rounded-md bg-(--foreground)">
-                    <p v-for="button in EDITOR_FOOTER_BUTTONS" :key="button.id" @click="() => { button.function(editorView) }" class="bg-white/15 px-4 rounded-sm">
+                    <p v-for="button in EDITOR_FOOTER_BUTTONS" :key="button.id" @pointerdown.stop @click="() => { button.function(editorView) }" class="bg-white/15 px-4 rounded-sm">
                         {{ button.label }}
                     </p>
                 </div>
             </ShinyContainer>
+            
         </div>
     </footer>
 </template>
