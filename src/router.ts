0c771c2550ddf9d87ba901a7c7f6b81af7a9e7b4
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2026-01-22 18:20:43 -0300

    deleted start page

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory, type RouteRecordRaw  } from "vue-router
 import HomeView from "./views/HomeView.vue";
 import FolderView from "./views/FolderView.vue";
 import FileView from "./views/FileView.vue";
-import StartView from "./views/StartView.vue";
 import type { RouteMeta } from "./types/route";
 import CodeNavbarMenu from "./components/layout/CodeNavbarMenu/CodeNavbarMenu.vue";
 
@@ -10,12 +9,6 @@ import CodeNavbarMenu from "./components/layout/CodeNavbarMenu/CodeNavbarMenu.vu
 const routes: Array<RouteRecordRaw & { meta: RouteMeta }> = [
   {
     path: "/",
-    name: "Start",
-    component: StartView,
-    meta: { navbarVariation: 'none' }
-  },
-  {
-    path: "/home",
     name: "Home",
     component: HomeView,
     meta: { navbarVariation: 'full' }
