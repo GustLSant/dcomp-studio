4434371df9b6e8d03bd2f5b83070cd2efa7394b9
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2026-01-29 15:43:14 -0300

    created components

---
+++ src/components/common/Logo.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+    import Logo from "../../assets/logo/white.png"
+    const props = defineProps<{ size?: string }>();
+</script>
+
+
+<template>
+    <img :src="Logo" alt="Logo do DcompStudio" class="themed-image" :style="{ width: props.size ?? 'auto' }" />
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/common/PageContainer.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+    <div class="flex flex-col gap-4 grow fade-in-left-short">
+        <slot />
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
