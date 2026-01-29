1f600c3e25a136956f19b539e417d198d17925bc
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2026-01-29 15:43:01 -0300

    text-white

--- src/components/common/CodePreview.vue
+++ src/components/common/CodePreview.vue
@@ -5,7 +5,7 @@
 
 <template>
     <div class="code-preview p-2 border border-(--border-01) rounded-sm">
-        <Icon icon="mdi:file-code-outline" width="36" height="36" />
+        <Icon icon="mdi:file-code-outline" class="text-white" width="36" height="36" />
     </div>
 </template>
 
