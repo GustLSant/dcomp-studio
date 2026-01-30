30eadb55d3b399ce780b9d62452ddfe072eb0611
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2026-01-29 21:47:40 -0300

    added iconClass prop

--- src/components/common/HoverableIcon.vue
+++ src/components/common/HoverableIcon.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
     import { Icon } from '@iconify/vue';
-    const props = defineProps<{ icon: string, size: number, disabled?: boolean }>();
+    const props = defineProps<{ icon: string, size: number, disabled?: boolean, iconClass?: string }>();
 </script>
 
 
 <template>
     <div class="border border-transparent p-1 rounded-full transition-colors duration-150 hover:border-black/20 dark:hover:border-white/20 hover:cursor-pointer select-none" :class="(props.disabled) ? 'opacity-40 pointer-events-none' : ''">
-        <Icon :icon="props.icon" :width="props.size" :height="props.size" />
+        <Icon :icon="props.icon" :width="props.size" :height="props.size" :class="props.iconClass" />
     </div>
 </template>
 
