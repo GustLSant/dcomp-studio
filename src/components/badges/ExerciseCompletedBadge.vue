41c1f5e177cd20ded4dbad6f815b1c33a9840bed
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2026-02-04 20:27:47 -0300

    created exercise badges

---
+++ src/components/badges/ExerciseCompletedBadge.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import { Icon } from '@iconify/vue';
+</script>
+
+
+<template>
+    <div class="flex items-center gap-1 text-(--success-color)">
+        <Icon icon="mdi:medal-outline" width="18" height="18" /> 
+        <p>Completo</p>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/badges/ExerciseIncompleteBadge.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import { Icon } from '@iconify/vue';
+</script>
+
+
+<template>
+    <div class="flex items-center gap-1 text-white/40">
+        <Icon icon="material-symbols:do-not-disturb-on-outline-rounded" width="18" height="18" /> 
+        <p>Incompleto</p>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
