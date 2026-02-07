e6a8b8af0475eed3acbf894e19fe65d24728c48e
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2026-02-07 13:49:33 -0300

    added ExerciseRecord type

--- src/types/exercises.ts
+++ src/types/exercises.ts
@@ -2,10 +2,16 @@ export type ExerciseData = {
     id: number,
     name: string,
     question: string,
-    completed: boolean,
     startCode: string[],
     codeAnswer: string[],
     expectedCodeOutput: string,
     studyMaterialName?: string,
     studyMaterialLink?: string,
+}
+
+export type ExerciseRecord = {
+    id?: number,
+    exerciseId: number,
+    completed: boolean,
+    fileIds: number[],
 }
