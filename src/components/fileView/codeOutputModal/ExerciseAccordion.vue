aa855213d5929d2772565bdf1a5d64cd2bc749a3
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2026-02-07 14:17:43 -0300

    Update ExerciseAccordion.vue

--- src/components/fileView/codeOutputModal/ExerciseAccordion.vue
+++ src/components/fileView/codeOutputModal/ExerciseAccordion.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
     import { Icon } from '@iconify/vue';
-    import type { ExerciseData } from '../../../types/exercises';
+    import type { ExerciseData, ExerciseRecord } from '../../../types/exercises';
     import ExerciseCompletedBadge from '../../badges/ExerciseCompletedBadge.vue';
     import ExerciseIncompleteBadge from '../../badges/ExerciseIncompleteBadge.vue';
-    import { ref } from 'vue';
+    import { computed, ref } from 'vue';
+    import { compareCodeOutputs, getExerciseDataById } from '../../../utils/exercises';
+    import type { CodeOutput } from '../../../types/code';
 
-    const props = defineProps<{ exerciseData: ExerciseData }>();
+    const props = defineProps<{ exerciseRecord: ExerciseRecord, codeOutput: CodeOutput }>();
+    const exerciseData = ref<ExerciseData>(getExerciseDataById(props.exerciseRecord.exerciseId));
+    const successOnThisTrial = computed(() => compareCodeOutputs(exerciseData.value.expectedCodeOutput, props.codeOutput.content));
+    
     const open = ref<boolean>(false);
 
     function toggleOpen() { open.value = !open.value; }
@@ -17,23 +22,22 @@
         <div @click="toggleOpen" class="flex items-center justify-between gap-2">
             <p>Exercício:</p>
             <div class="flex items-center gap-1">
-                <p>{{ props.exerciseData.name }}</p>
-                <Icon v-if="props.exerciseData.completed" class="text-(--success-color)" icon="mdi:check-circle-outline" width="18" height="18" />
+                <p>{{ exerciseData.name }}</p>
+                <Icon v-if="successOnThisTrial" class="text-(--success-color)" icon="mdi:check-circle-outline" width="18" height="18" />
                 <Icon :icon="(open) ? 'mdi:chevron-up' : 'mdi:chevron-down'" width="24" height="24" />
             </div>
         </div>
 
         <div class="flex flex-col gap-1">
-            <p>Output esperado:</p>
+            <p class="text-sm">Output esperado:</p>
             <div class="bg-neutral-950 p-2 rounded-sm shadow-inner font-mono text-green-500">
-                <p class="whitespace-pre">{{ props.exerciseData.expectedCodeOutput }}</p>
+                <p class="whitespace-pre">{{ exerciseData.expectedCodeOutput }}</p>
             </div>
         </div>
 
         <div class="flex items-center justify-between gap-2">
             <p>Status:</p>
-
-            <ExerciseCompletedBadge v-if="props.exerciseData.completed" class="text-success" />
+            <ExerciseCompletedBadge v-if="successOnThisTrial" class="text-success" />
             <ExerciseIncompleteBadge v-else />
         </div>
     </div>
