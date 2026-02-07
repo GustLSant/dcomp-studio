4ef0f1a6ba6c8ecbd14e7bc2b0e60507298ed7bb
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2026-02-07 13:49:54 -0300

    added new exercise

--- src/data/exercises.ts
+++ src/data/exercises.ts
@@ -11,7 +11,6 @@ export const EXERCISES: ExerciseData[] = [
             '# Para printar o valor, utilize print(x)',
             '# Output esperado: "10"',
         ],
-        completed: false,
         codeAnswer: [
             'x = 10',
             'print(x)'
@@ -23,13 +22,31 @@ export const EXERCISES: ExerciseData[] = [
     {
         id: 1,
         name: 'Variáveis 2',
+        question: 'Crie três variáveis do tipo string com os valores "estudo", "de" e "programação", e imprima uma frase com essas três strings ao mesmo tempo.',
+        startCode: [
+            '# Crie três variáveis do tipo string com os valores "estudo", "de" e "programação"',
+            '# Imprima uma frase com essas três strings ao mesmo tempo',
+            '# Output esperado: "estudo de programação"',
+        ],
+        codeAnswer: [
+            'a = "estudo"',
+            'b = "de"',
+            'c = "programação"',
+            'print(a,b,c)'
+        ],
+        expectedCodeOutput: 'estudo de programação',
+        studyMaterialName: 'docs.python',
+        studyMaterialLink: 'https://docs.python.org/3/library/string.html'
+    },
+    {
+        id: 2,
+        name: 'Variáveis 3',
         question: 'Crie uma variável do tipo "lista" com os valores: 1, 2, 3 e imprima a lista criada.',
         startCode: [
             '# Crie uma variável do tipo lista com os valores: 1, 2, 3',
             '# Imprima a lista criada',
             '# Output esperado: "[1, 2, 3]"',
         ],
-        completed: false,
         codeAnswer: [
             'lista = [1,2,3]',
             'print(lista)'
