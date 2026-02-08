46d81fbf8bd1d8e48bab9a28fffd72b4b587a13c
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2026-02-08 01:28:19 -0300

    visual rework

--- src/components/fileView/CodeNavbarMenu/EditorThemeAccordion.vue
+++ src/components/fileView/CodeNavbarMenu/EditorThemeAccordion.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
-    import { ref } from 'vue';
     import { editorThemeItems, type EditorTheme } from '../../../enums/editorThemes';
     import { changeEditorTheme } from '../../../utils/editorTheme';
-    import { Icon } from '@iconify/vue';
-
-    const isOpen = ref<boolean>(false);
+    import Accordion from '../../common/Accordion.vue';
 
     function handleClickChangeEditorTheme(_theme: EditorTheme) {
         changeEditorTheme(_theme);
@@ -13,14 +10,13 @@
 
 
 <template>
-    <section @click.stop="() => { isOpen = !isOpen; }" :class="(isOpen) ? 'max-h-[600px]' : 'max-h-10'" class="overflow-y-hidden border border-white/20 rounded-md p-2 gap-2!">
-        <div class="flex items-center justify-between">
-            <p>Tema do Editor:</p>
-            <Icon v-if="isOpen" icon="mdi:chevron-up" width="24" height="24" />
-            <Icon v-else icon="mdi:chevron-down" width="24" height="24" />
-        </div>
-        <div v-for="theme in editorThemeItems" @click.stop="() => { handleClickChangeEditorTheme(theme) }" class="bg-neutral-600 p-2 rounded-sm">{{ theme }}</div>
-    </section>
+    <Accordion title="Tema do Editor:" max-height="460px" min-height="44px" class="py-1!">
+        <template #body>
+            <div class="flex flex-col gap-2 pb-1.5">
+                <div v-for="theme in editorThemeItems" @click.stop="() => { handleClickChangeEditorTheme(theme) }" class="border border-(--border-02) bg-white/10 p-2 rounded-sm">{{ theme }}</div>
+            </div>
+        </template>
+    </Accordion>
 </template>
 
 
