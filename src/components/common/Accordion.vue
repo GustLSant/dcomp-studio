04524c6beeb677f00d314f1282e9e1d4daf20018
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2026-02-08 14:58:46 -0300

    added headerClassOverride and header prefix/sulfix

--- src/components/common/Accordion.vue
+++ src/components/common/Accordion.vue
@@ -8,6 +8,7 @@
         icon?: string,
         minHeight?: string,
         maxHeight: string,
+        headerClassOverride?: string,
     }>();
 
     const isOpen = ref<boolean>(false);
@@ -19,13 +20,17 @@
         class="accordion flex flex-col gap-2 p-2 overflow-y-hidden border border-(--border-02) rounded-md"
         :style="{ maxHeight: isOpen ? props.maxHeight : (props.minHeight ?? '50px') }"
     >
-        <div @click.stop="() => { isOpen = !isOpen }" class="flex items-center justify-between gap-2 hover:cursor-pointer">
+        <div @click.stop="() => { isOpen = !isOpen }" class="flex items-center justify-between gap-2 hover:cursor-pointer" :class="props.headerClassOverride">
             <div class="flex items-center gap-2">
                 <Icon v-if="props.icon" :icon="props.icon" width="24" height="24" />
                 <p :class="(!props.icon) ? 'pl-2' : ''">{{ props.title }}</p>
+                <slot name="header-prefix" />
             </div>
 
-            <HoverableIcon :icon="(!isOpen) ? 'mdi:chevron-down' : 'mdi:chevron-up'" :size="24" />
+            <div class="flex items-center gap-1">
+                <slot name="header-sulfix" />
+                <HoverableIcon :icon="(!isOpen) ? 'mdi:chevron-down' : 'mdi:chevron-up'" :size="24" />
+            </div>
         </div>
 
         <div class="flex flex-col gap-2 overflow-y-auto">
